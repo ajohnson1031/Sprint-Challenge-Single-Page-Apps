f5f0d4e3851c2fdf69e337c2f30a6cd63b757837
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-08-17 13:07:44 -0400

    set up initial card and cardlist component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { Card, Image, Icon, Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <Card key={props.char.id}>
+      <Image src={props.char.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{props.char.name}</Card.Header>
+        <Card.Meta>
+          <span>
+            {props.char.species} {props.char.status}
+          </span>
+        </Card.Meta>
+        <Card.Description>
+          <p>
+            <strong>Location:</strong> {props.char.location.name}
+          </p>
+          <p>
+            <strong>Origin:</strong> {props.char.origin.name}
+          </p>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="film" />
+          Episodes: {props.char.episode.length}
+        </a>
+      </Card.Content>
+    </Card>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState();
+  const [next, setNext] = useState();
+  const [prev, setPrev] = useState();
 
   useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log(res);
+        setCharacters(res.data.results);
+        setNext(res.info.next);
+        setPrev(res.info.prev);
+      })
+      .catch(err => console.log(err));
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters &&
+        characters.map((c, i) => <CharacterCard char={c} key={i} />)}
     </section>
   );
 }
